c94a5dcc45e45e9e1ce636a00f4a50dc6297cc07
Ashad Syed Muhammad Ahmed <63679335+ashad767@users.noreply.github.com>
2026-06-08 15:06:56 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # s s s s s s
+        if n == 1:
+            return 1
+
+        stairs = [0] * n
+        stairs[0] = 1
+        stairs[1] = 2
+
+        for i in range(2, n):
+            stairs[i] = stairs[i - 1] + stairs[i - 2]
+        
+        return stairs[n-1]
